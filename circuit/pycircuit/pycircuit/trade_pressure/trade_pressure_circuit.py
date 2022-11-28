a09d743e00d79da518e06be2d482a6eccc5a026a
vgatherps <vgatherps@gmail.com>
2022-11-28 20:19:23 +0800

    Turn into one large sum for now

--- circuit/pycircuit/pycircuit/trade_pressure/trade_pressure_circuit.py
+++ circuit/pycircuit/pycircuit/trade_pressure/trade_pressure_circuit.py
@@ -6,6 +6,8 @@ from pycircuit.trade_pressure.trade_pressure_config import (
 )
 from tree_sum import tree_sum
 
+from pycircuit.pycircuit.circuit_builder.circuit import OutputOptions
+
 
 def generate_circuit_for_market_venue(
     circuit: CircuitBuilder, market: str, venue: str, config: TradePressureVenueConfig
@@ -36,14 +38,21 @@ def generate_circuit_for_market(
         all_ticks.append(tick)
         all_running.append(running)
 
-    running_sum = tree_sum(circuit, all_running)
-    ticks_sum = tree_sum(circuit, all_ticks)
-
-    return running_sum, ticks_sum
+    return all_running + all_ticks
 
 
 def generate_trade_pressure_circuit(
     circuit: CircuitBuilder, config: TradePressureConfig
 ) -> CircuitData:
+
+    all_sums = []
+    for (market, market_config) in config.markets.items():
+        all_sums += generate_circuit_for_market(circuit, market, market_config)
+
+    total_sum = tree_sum(circuit, all_sums)
+
+    circuit.components[total_sum.parent].output_options["out"] = OutputOptions(
+        force_stored=True
+    )
 
     return circuit
