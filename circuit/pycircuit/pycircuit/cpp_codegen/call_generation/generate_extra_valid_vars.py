7465154e4cd2e09f023c99c3a40886a92b218fbf
vgatherps <vgatherps@gmail.com>
2022-12-09 17:58:34 +0800

    Generate invalidations for unwritten assume_invalid inputs

--- circuit/pycircuit/pycircuit/cpp_codegen/call_generation/generate_extra_valid_vars.py
+++ circuit/pycircuit/pycircuit/cpp_codegen/call_generation/generate_extra_valid_vars.py
@@ -1,22 +1,18 @@
 from typing import List
 
-from pycircuit.circuit_builder.circuit import ComponentInput
-from pycircuit.circuit_builder.definition import CallSpec, OutputSpec
-from pycircuit.cpp_codegen.call_generation.call_data import CallData
-from pycircuit.cpp_codegen.call_generation.callset import get_inputs_for_callset
+from pycircuit.circuit_builder.definition import OutputSpec
 from pycircuit.cpp_codegen.call_generation.find_children_of import CalledComponent
 from pycircuit.cpp_codegen.call_generation.single_call.generate_output_calldata import (
     get_valid_path,
 )
-from pycircuit.cpp_codegen.generation_metadata import (
-    AnnotatedComponent,
-    GenerationMetadata,
-)
-from pycircuit.cpp_codegen.type_names import get_type_name_for_input
+from pycircuit.cpp_codegen.generation_metadata import GenerationMetadata
+
 
 # This is sort of duplicated?
 # For always valid outputs that *are not* written as part of the call tree
 # Generate something referencing true
+# TODO does it make sense to generate all these validity vars upfront,
+# and not worry about it in the function logic?
 def generate_extra_validity_references(
     children_for_call: List[CalledComponent], metadata: GenerationMetadata
 ) -> str:
@@ -30,17 +26,25 @@ def generate_extra_validity_references(
     input_with_component = [
         (input, metadata.annotated_components[input.parent])
         for input in requested_inputs
-        if input.parent != 'external'
+        if input.parent != "external"
     ]
 
-    static_defs = [
+    static_valid_defs = [
         f"constexpr bool {get_valid_path(component, input.output_name)} = true;"
         for (input, component) in input_with_component
         if (input.parent not in called_children_names)
-        and component.component.definition.d_output_specs.get(
-            input.output_name,
-            OutputSpec(ephemeral=False, type_path="", always_valid=False),
-        ).always_valid
+        and component.component.definition.d_output_specs[
+            input.output_name
+        ].always_valid
     ]
 
-    return "\n".join(static_defs)
+    static_invalid_defs = [
+        f"constexpr bool {get_valid_path(component, input.output_name)} = false;"
+        for (input, component) in input_with_component
+        if (input.parent not in called_children_names)
+        and component.component.definition.d_output_specs[
+            input.output_name
+        ].assume_invalid
+    ]
+
+    return "\n".join(static_valid_defs + static_invalid_defs)
