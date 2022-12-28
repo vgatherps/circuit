97a998eeab867ae8ff6d1f60e5e95e923be550ab
vgatherps <vgatherps@gmail.com>
2022-12-28 16:35:47 +0800

    Add some stuff to do things

--- circuit/cppcuit/include/signals/bookbuilder.hh
+++ circuit/cppcuit/include/signals/bookbuilder.hh
@@ -2,9 +2,26 @@
 
 #include <concepts>
 #include <functional>
-#include <absl/container/btree_map.h>
 
-#include "md_types/depth_message_generated.h"
+#include <absl/container/btree_map.h>
+#include <flatbuffers/flatbuffers.h>
+
+#include "md_types/common_generated.h"
+
+enum class LevelDecision {
+    Keep,
+    Discard
+};
+
+template<class F>
+concept LevelUpdater = requires(F f, Side s, Level l, Metadata& m) {
+    {f(s, l, m)} -> std::same_as<LevelDecision>;
+};
+
+template<class F>
+concept LevelCreator = concept LevelUpdater = requires(F f, Side s, Level l) {
+    {f(s, l)} -> std::same_as<std::optional<Metadata>>;
+};
 
 // Bookbuilder with attached state to each level
 
@@ -15,9 +32,9 @@ class BookBuilder {
 
     public:
 
-    template<class F>
-    requires std::invokable<F, Side, Level, &Metadata>
-    void update_levels(const DepthMessage *depth, const F& fnc) {
-
+    template<LevelCreator C, LevelUpdater U>
+    void update_levels(const DepthUpdate *depth, const C& update, const U& creator) {
+        const auto &bids = *depth.bids();
+        const auto &asks = *depth.asks();
     }
 };
