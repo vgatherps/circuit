4dabaed0cfb9708bb9bc60cb417f3a539f2c8002
vgatherps <vgatherps@gmail.com>
2023-02-05 18:41:05 +0800

    Fix minor error in example program

--- circuit/pycircuit/pycircuit/differentiator/examples/basic_linreg/main.py
+++ circuit/pycircuit/pycircuit/differentiator/examples/basic_linreg/main.py
@@ -19,7 +19,7 @@ def main():
     graph = Graph.discover_from_circuit(gen.circuit, gen.regress)
 
     pprint(graph.pretty())
-    model = Model(graph)
+    model = Model(graph, )
 
     print(model.edges())
 
@@ -27,13 +27,13 @@ def main():
     x_output = ComponentOutput(parent='external', output_name='x')
 
     real_x = np.array([1, 2, 3, 4, 5])
-    real_y = np.array([-1, -2, -3, -4, -5])
+    real_y = np.array([-4, -2, -1, 4, -5])
 
     real_results = REAL_X_MUL * real_x + REAL_Y_MUL * real_y
 
     data = {
-        y_output: make_constant(real_x),
-        x_output: make_constant(real_y),
+        x_output: make_constant(real_x),
+        y_output: make_constant(real_y),
     }
 
     optim = SGD(model.parameters_list(), lr=0.01)
@@ -43,34 +43,36 @@ def main():
 
     loss = torch.nn.MSELoss()
 
+    def report(projected, loss):
+        fxp = float(x_param)
+        fyp = float(y_param)
+
+        print("Computed loss: ", float(loss))
+
+        print(f"Real X factor: {REAL_X_MUL}, computed X factor: {fxp}")
+        print(f"Real Y factor: {REAL_Y_MUL}, computed Y factor: {fyp}")
+
     for _ in range(0, 100):
 
         projected = model.evaluate_on(data)
-
 
         computed_loss = loss(projected, make_constant(real_results))
 
         optim.zero_grad()
         computed_loss.backward()
 
-        print(computed_loss)
+        report(projected, computed_loss)
 
-        print("Before step")
-        print(x_param)
-        print(y_param)
+        if computed_loss < 1e-3:
+            break
+
+        print()
+        print()
 
         optim.step()
 
-        print()
-        print("After step")
-
-    projected = model.evaluate_on(data)
-
     print("Real results: ", real_results)
-    print("Projected results: ", projected)
-
-    print(f"Real X factor: {REAL_X_MUL}, computed X factor: {float(x_param)}")
-    print(f"Real Y factor: {REAL_Y_MUL}, computed Y factor: {float(y_param)}")
+    print("Projected results: ", projected.detach().numpy())
 
 if __name__ == "__main__":
     main()
